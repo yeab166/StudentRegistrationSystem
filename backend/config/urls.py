a558e793344bdec8a1300f8c8504ace543733697
Yeabsira Belete <yeababa16@gmail.com>
2026-01-03 23:33:46 +0300

    Delete backend directory

--- backend/config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from routes.student_routes import urlpatterns as student_urls
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(student_urls)),
-]
--- backend/routes/student_routes.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from controllers.student_controller import StudentAPI
-
-urlpatterns = [
-    path('students/', StudentAPI.as_view()),
-]
